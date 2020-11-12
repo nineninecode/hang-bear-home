1fd84381d91e49cfe79d6d106591f9db57416231
wzh <1203731742@qq.com>
2020-11-12 23:53:17 +0800

    feat(security): 自定义登录

--- src/main/java/com/wzh/home/HangBearHomeApplication.java
+++ src/main/java/com/wzh/home/HangBearHomeApplication.java
@@ -3,6 +3,14 @@ package com.wzh.home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * spring boot 启动类
+ * </p>
+ *
+ * @author weizhuohang
+ * @since 2020/10/30 15:14
+ */
 @SpringBootApplication
 public class HangBearHomeApplication {
 
---
+++ src/main/java/com/wzh/home/config/security/MyInterceptor.java
@@ -0,0 +1,12 @@
+package com.wzh.home.config.security;
+
+/**
+ * <p>
+ * 代码描述
+ * </p>
+ *
+ * @author weizhuohang
+ * @since 2020/11/12 23:12
+ */
+public class MyInterceptor {
+}
