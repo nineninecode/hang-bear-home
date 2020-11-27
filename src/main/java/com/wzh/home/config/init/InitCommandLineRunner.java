508cf4acd0d88ab76fb3e8026da90ab7d00863dc
weizhuohang <weizh4@bngrp.com>
2020-11-27 16:54:50 +0800

    spring security

---
+++ src/main/java/com/wzh/home/config/init/InitCommandLineRunner.java
@@ -0,0 +1,34 @@
+package com.wzh.home.config.init;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.wzh.home.config.security.CustomIgnoreUrlProperties;
+
+/**
+ * <p>
+ * spring boot 初始化完毕后init
+ * </p>
+ *
+ * @author weizhuohang
+ * @since 2020/11/27 16:47
+ */
+@Slf4j
+@Component
+public class InitCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private CustomIgnoreUrlProperties customIgnoreUrlProperties;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("url {}", customIgnoreUrlProperties.getUrl());
+        log.info("url-array {}", Arrays.toString(customIgnoreUrlProperties.getUrlArray()));
+        log.info("url-list {}", customIgnoreUrlProperties.getUrlList());
+    }
+}
